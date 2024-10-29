e11ee453232a1b9831ae800558cca6063d8f8a71
mykerl-ai <michaelchigozie51@gmail.com>
2024-10-29 19:56:27 +0100

    report

--- src/views/FindRole.vue
+++ src/views/FindRole.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="h-screen w-screen bg-black flex flec-col overflow-y-auto">
     <div
-      class="w-full space-y-10 px-10 py-10 block h-full md:flex md:w-full md:h-full md:flex-row"
+      class="w-full space-y-10 px-10 py-10 block h-full md:flex md:w-full md:h-full md:flex-col"
     >
-      <div>
+      <div class="flex items-center justify-between">
         <p>List</p>
+
+        <button
+          @click="downloadExcel"
+          type="submit"
+          class="w-84 py-4 bg-[#62CD9A] rounded-3xl focus:outline-none"
+        >
+          <span class="text-white">Download report</span>
+        </button>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-y-3 gap-x-6">
@@ -35,6 +43,7 @@
 </template>
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import * as XLSX from "xlsx/xlsx.mjs";
 
 const data = ref({});
 import { useDataStore } from "@/stores/data";
@@ -66,6 +75,43 @@ const fetchRole = async () => {
     loading.value = false;
   }
 };
+
+function downloadExcel() {
+  // Create a new workbook and add a worksheet
+  const data = getRoles.value.map((item) => {
+    delete item.__typename;
+    delete item.id;
+    return item;
+  });
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(data);
+
+  // Append the worksheet to the workbook
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+  // Generate a binary string representing the workbook
+  const excelData = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+
+  // Create a Blob from the workbook data
+  const blob = new Blob([excelData], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  // Create a link element
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "report.xlsx";
+
+  // Append the link to the body temporarily and click it
+  document.body.appendChild(link);
+  link.click();
+
+  // Remove the link from the DOM after download
+  document.body.removeChild(link);
+
+  // Clean up by revoking the URL object
+  setTimeout(() => URL.revokeObjectURL(link.href), 100);
+}
 onMounted(async () => {
   await fetchRole();
 });
